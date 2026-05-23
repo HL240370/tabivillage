d57d27feb9c8acfc1128a8087503883bc2582fdf
heon <dlgjs7183@gmail.com>
2026-05-24 04:33:05 +0900

    feature/TYPEデータ設定

--- src/data/levelTable.ts
+++ src/data/levelTable.ts
@@ -1,8 +1,29 @@
-export type LevelTableEntry = {
-  level: number;
-  requiredExp: number;
-  maxHouseCount: number;
-};
+import { LevelTableEntry } from "@/types/level";
 
-export const levelTable: LevelTableEntry[] = [];
-
+export const levelTable: LevelTableEntry[] = [
+  {
+    level: 1,
+    requiredTotalExp: 0,
+    maxHouses: 1,
+  },
+  {
+    level: 2,
+    requiredTotalExp: 120,
+    maxHouses: 2,
+  },
+  {
+    level: 3,
+    requiredTotalExp: 320,
+    maxHouses: 3,
+  },
+  {
+    level: 4,
+    requiredTotalExp: 650,
+    maxHouses: 4,
+  },
+  {
+    level: 5,
+    requiredTotalExp: 1100,
+    maxHouses: 5,
+  },
+] as const;
--- src/types/house.ts
+++ src/types/house.ts
@@ -1,6 +1,11 @@
 export type HouseData = {
   id: string;
+  name : string;
   level: number;
   skinId?: string;
+  region: string;
+  goldPerHour: number;
+  expPerHour: number;
+  inUnlocked: boolean;
 };
 
